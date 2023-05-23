cdffbf09586545734233a3a11530211d56bf813d
Jake Hyvonen <jakehyvonen@gmail.com>
2023-05-23 17:24:45 -0400

    work towards integration - mostly functional

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const cors = require("cors");
 const path = require("path");
+var converter = require('./input_converter');
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
@@ -35,6 +36,30 @@ io.on('connection',(socket)=>{
   socket.on('disconnect',(reason)=>{
     console.log(reason)
   })
+
+
+  socket.on('action_keydown', function (action_data)
+  {
+    console.log('got action_keydown: ' + action_data);
+    converter.handle_action_keydown(action_data);
+  });
+  socket.on('keyboard_input', function (keyboard_data)
+  {
+    console.log('got keyboard_data: ' + keyboard_data);
+    converter.handle_keyboard_data(keyboard_data);
+  });
+  socket.on('joystick_input', function(joystick_data)
+  {
+    console.log('got joystick_data: ' + joystick_data);
+    converter.handle_joystick_data(joystick_data);
+  });
+  
+  socket.on('device_orientation', function(device_orientation_data)
+  {
+    console.log('got device_orientation_data: ' + device_orientation_data);
+    converter.handle_device_orientation_data(device_orientation_data);
+  });
+
 })
 
 
